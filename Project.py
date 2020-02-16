f754a990b2b0245615701ec52c740c40f06f0c85
LI-BOY1 <boyli@boylis-mbp.myfiosgateway.com>
2020-02-15 21:16:11 -0500

    add the parsing

--- Project.py
+++ Project.py
@@ -1,4 +1,45 @@
+import datetime
+
+personList = []
+familyList = []
+
+
+flag = -1
+birth = False
+death = False
+marry = False
+div = False
+
+
+class Person():
+
+    def _init_(self):
+        self.id = ''
+        self.name = ""
+        self.gender = ""
+        self.birthDate = ""
+        self.age = -1
+        self.alive = True
+        self.death = "NA"
+        self.child = []
+        self.spouse = []
+
+class Family():
+
+    def __init__(self):
+        self.id = ''
+        self.married = ''
+        self.divorce = 'NA'
+        self.husbandID = ''
+        self.husbandName = ''
+        self.wifeID = ''
+        self.wifeName = ''
+        self.chidren = []
+
+
 def process_line(line):
+
+    global flag, birth, death, marry, div, personList, familyList
     valid_tages = {
         '0': ('HEAD', 'NOTE', 'TRLR'),
         '1': ('BIRT', 'CHIL', 'DEAT', 'DIV', 'FAMC', 'FAMS', 'HUSB', 'MARR', 'NAME', 'SEX', 'WIFE'),
@@ -11,17 +52,98 @@ def process_line(line):
     if len(tokens) == 3 and tokens[0] == '0' and tokens[2] in ('INDI', 'FAM'):
         level, args, tag = tokens
         valid = 'Y'
+
+        #set the person flag
+        if tokens[2] == 'INDI':
+            flag = 0
+            person = Person()
+
+
+            # print(f"-->{line}")
+
+
+            person.id = tokens[1]
+            person.child = []
+            person.spouse = []
+            personList.append(person)
+
+            # attrs = vars(person)
+            # print(',  '.join("%s: %s" % item for item in attrs.items()))
+
+
+        else:
+            flag = 1
+            family = Family();
+            family.id = tokens[1]
+            family.chidren = []
+
+            familyList.append(family)
+
     elif len(tokens) >= 2:
         level, tag, args = tokens[0], tokens[1], " ".join(tokens[2:])
         valid = 'Y' if level in valid_tages and tag in valid_tages[level] else 'N'
+
+        if(valid == 'Y'):
+
+            if(level == "2"):
+                if(birth):
+                    birth = False
+                    personList[-1].birthDate = args
+
+                if(death):
+                    death = False
+                    personList[-1].death = args
+                    personList[-1].alive = False
+
+                if(marry):
+                    marry = False
+                    familyList[-1].married = args
+
+                if(div):
+                    div = False
+                    familyList[-1].divorce = args
+
+            # deal with person
+            if(flag == 0):
+                if tokens[1] == "NAME":
+                    personList[-1].name = args
+                elif tokens[1] == "SEX":
+                    personList[-1].gender = args
+                elif tokens[1] == "BIRT":
+
+                    birth = True
+                elif tokens[1] ==  "DEAT":
+
+                    death = True
+                elif tokens[1] == "FAMC":
+                    personList[-1].child.append(args)
+
+                elif tokens[1] == "FAMS":
+                    personList[-1].spouse.append(args)
+            # deal wiht family
+            else:
+                if(tokens[1] == "MARR"):
+
+                    marry = True
+                elif (tokens[1] == "DIV"):
+
+                    div = True
+                elif ( tokens[1] == "HUSB"):
+                    familyList[-1].husbandID = args
+                elif (tokens[1] == "WIFE"):
+                    familyList[-1].wifeID = args
+                elif (tokens[1] == "CHIL"):
+                    familyList[-1].chidren.append(args)
+
+
     else:
         level, tag, valid, args = tokens, "NA", 'N', 'NA'
 
-    print(f"<-- {level}|{tag}|{valid}|{args}")
+    # print(f"<-- {level}|{tag}|{valid}|{args}")
 
 
 def main():
-    file = "/Users/boyli/Desktop/Boyang Li.ged"
+    file = "/Users/boyli/Desktop/proj02test.ged"
 
     try:
         fp = open(file);
@@ -31,6 +153,41 @@ def main():
         with fp:
             for line in fp:
                 process_line(line.strip())
+
+    global flag, birth, death, marry, div, personList, familyList
+
+    now = datetime.datetime.now();
+    year = now.year
+
+    # calculate the age for each person
+    for p in personList:
+        birthdate = p.birthDate
+        birthYear = birthdate.split()[2]
+        age = year - int(birthYear)
+        p.age = age
+
+    #set the names
+    for f in familyList:
+        husId = f.husbandID
+        wifeId = f.wifeID
+        for p in personList:
+            if(husId == p.id):
+                f.husbandName = p.name
+            if(wifeId == p.id):
+                f.wifeName = p.name
+
+
+
+
+
+
+    for p in personList:
+        attrs = vars(p)
+        print(',  '.join("%s: %s" % item for item in attrs.items()))
+
+    for f in familyList:
+        attrs = vars(f)
+        print(',  '.join("%s: %s" % item for item in attrs.items()))
 
 
 
