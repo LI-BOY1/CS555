393fb6dc6b83ce88bfab6f0a33355d30308bcae4
weichen66 <34478443+weichen66@users.noreply.github.com>
2020-02-28 20:23:55 -0500

    Add files via upload

---
+++ test_us08.py
@@ -0,0 +1,13 @@
+import unittest
+from us08 import us08
+import Project
+
+Project.main()
+
+class test_us08(unittest.TestCase):
+    def test_us08(self):
+        self.assertEqual(us08(), 'The data in ged_file is Right. (us08)')
+        # self.assertEqual(us08(), 'The data in ged_file is Wrong. (us08)')
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)    
---
+++ us08.py
@@ -0,0 +1,40 @@
+import Project
+from datetime import datetime
+
+def us08():
+    # Birth before marriage of parents
+    """
+    Children should be born after marriage of parents 
+    (and not more than 9 months after their divorce)
+    """
+
+    flag = True
+    for person in Project.personList:
+        key = person.child
+        ky = "".join(key)
+        birth_date = datetime.strptime(datetime.strptime(person.birthDate, '%d %b %Y').strftime("%Y-%m-%d"), '%Y-%m-%d').date()
+        for i in range(len(Project.familyList)):
+            if Project.familyList[i].id == ky:
+                married_date = datetime.strptime(datetime.strptime(Project.familyList[i].married, '%d %b %Y').strftime("%Y-%m-%d"), '%Y-%m-%d').date()
+                if married_date > birth_date:
+                    flag = False
+                    
+                divorce = Project.familyList[i].divorce
+                if divorce != 'NA':
+                    divorce_date = datetime.strptime(datetime.strptime(Project.familyList[i].divorce, '%d %b %Y').strftime("%Y-%m-%d"), '%Y-%m-%d').date()
+                    dif_time = ((birth_date - divorce_date).days/365.25) * 12
+                    # print(dif_time)
+                    if dif_time > 9:
+                        flag = False
+    if flag:
+        return "The data in ged_file is Right. (us08)"
+    else:
+        return "The data in ged_file is Wrong. (us08)"
+
+def main():
+    Project.main()
+    print(us08())
+
+if __name__ == "__main__":
+    main()
+
