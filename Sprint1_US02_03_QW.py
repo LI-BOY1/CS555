2c703371e3bf7d4d13fd159ccc94ad8baee805c4
FH18 <48007526+wwwqllll@users.noreply.github.com>
2020-02-29 10:09:58 -0500

    add return value to user story function

--- Sprint1_US02_03_QW.py
+++ Sprint1_US02_03_QW.py
@@ -6,6 +6,7 @@ def US02(out_file, personList, familyList):
     by qw
     To check if there is any violation of "Birth should occur before marriage of an individual"
     """
+    error = 0
     for f in familyList:
         for p in personList:
 
@@ -13,6 +14,7 @@ def US02(out_file, personList, familyList):
                 birth_date = datetime.datetime.strptime(p.birthDate, '%d %b %Y').date() 
                 marry_date = datetime.datetime.strptime(f.married, '%d %b %Y').date()
                 if birth_date > marry_date:
+                    error += 1
                     with open(out_file, 'a') as file:
                         error_msg = "ERROR: FAMILY: US02: " + f.id + " Wife's birth date " + str(birth_date) + " following marriage date " + str(marry_date)
                         file.write(error_msg)
@@ -22,10 +24,13 @@ def US02(out_file, personList, familyList):
                 birth_date = datetime.datetime.strptime(p.birthDate, '%d %b %Y').date() 
                 marry_date = datetime.datetime.strptime(f.married, '%d %b %Y').date()
                 if birth_date > marry_date:
+                    error += 1
                     with open(out_file, 'a') as file:
                         error_msg = "ERROR: FAMILY: US02: " + f.id + " Husband's birth date " + str(birth_date) + " following marriage date " + str(marry_date)
                         file.write(error_msg)
-                        file.write('\n')             
+                        file.write('\n')
+
+    return error   
 
 
 def US03(out_file, personList):
@@ -33,7 +38,7 @@ def US03(out_file, personList):
     by qw
     To check if there is any violation of "Birth should occur before death of an individual"
     """
-
+    error = 0
     for p in personList:
         if p.death == "N/A":
             continue
@@ -41,9 +46,11 @@ def US03(out_file, personList):
         death_date = datetime.datetime.strptime(p.death, '%d %b %Y').date()
         birth_date = datetime.datetime.strptime(p.birthDate, '%d %b %Y').date()
         if birth_date > death_date:
+            error += 1
             with open(out_file, 'a') as file:
                 error_msg = "ERROR: INDIVIDUAL: US03: " + p.id + " Died " + str(death_date) + " before born " + str(birth_date)
                 file.write(error_msg)
                 file.write('\n')
+    return error   
 
          
