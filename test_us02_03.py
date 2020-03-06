d3494461938d1e1f39c5e0e8b464bb537725d8a2
FH18 <48007526+wwwqllll@users.noreply.github.com>
2020-03-05 21:41:45 -0500

    add us3 errors

--- test_us02_03.py
+++ test_us02_03.py
@@ -1,64 +1,62 @@
+"""
+by qw
+Test for Sprint1 US02 03
+"""
+
 import datetime
+import unittest
+from SSW555_project import Person, Family, US02, US03
 
-def compare_date(date1, date2):
-    """
-    by qw
-    """
-    c_date1 = datetime.datetime.strptime(date1, '%d %b %Y').date() 
-    c_date2 = datetime.datetime.strptime(date2, '%d %b %Y').date()
-    if c_date1 > c_date2:
-        return 1
-    elif c_date1 < c_date2:
-        return -1
-    else:
-        return 0
-
-
-def US02(out_file, personList, familyList, print_flag=True):
-    """
-    by qw
-    To check if there is any violation of "Birth should occur before marriage of an individual"
-    out_file: the path of output file
-    print_flag: boolean, whether the function output to a file or not 
-    """
-    error = 0
-    for f in familyList:
-        for p in personList:
-
-            if p.id == f.wifeID:
-                if compare_date(p.birthDate, f.married) == 1:
-                    error += 1
-                    if print_flag:
-                        print("ERROR: FAMILY: US02: " + f.id + " Wife's birth date " + str(birth_date) + " following marriage date " + str(marry_date))
-
-            if p.id == f.husbandID:
-                if compare_date(p.birthDate, f.married) == 1:
-                    error += 1
-                    if print_flag:
-                            print(error_msg = "ERROR: FAMILY: US02: " + f.id + " Husband's birth date " + str(birth_date) + " following marriage date " + str(marry_date))
-                            
-    return error   
-
-
-def US03(personList, print_flag=True):
-    """
-    by qw
-    To check if there is any violation of "Birth should occur before death of an individual"
-
-    print_flag: boolean, whether the function output to a file or not 
-    """
-    error = 0
-    for p in personList:
-        if p.death == "N/A":
-            continue
+class Test_US02_03(unittest.TestCase):
+    
+    def test_US02_03(self):
+        """ test US02 and US03"""
+        test_person1 = Person()
+        test_person1.id = "I01"
+        test_person1.name = "Marry Sue"
+        test_person1.gender = "F"
+        test_person1.birthDate = "29 SEP 1993"
+        test_person1.age = 27
+        test_person1.alive = True
+        test_person1.death = "23 JAN 2020"
+        test_person1.child = []
+        test_person1.spouse = ["I02"]
         
-        death_date = datetime.datetime.strptime(p.death, '%d %b %Y').date()
-        birth_date = datetime.datetime.strptime(p.birthDate, '%d %b %Y').date()
-        if compare_date(p.birthDate, p.death) == 1:
-            error += 1
-            if print_flag:
-                print("ERROR: INDIVIDUAL: US03: " + p.id + " Died " + str(death_date) + " before born " + str(birth_date))
+        test_person2 = Person()
+        test_person2.id = "I02"
+        test_person2.name = "Morgan Sue"
+        test_person2.gender = "M"
+        test_person2.birthDate = "12 Jan 1988"
+        test_person2.age = 32
+        test_person2.alive = False
+        test_person2.death = "23 JAN 2020"
+        test_person2.child = []
+        test_person2.spouse = ["I01"]
 
-    return error   
+        test_family = Family()
+        test_family.id = "F01"
+        test_family.married = "29 SEP 1993"
+        test_family.divorce = "NA"
+        test_family.husbandID = "I02"
+        test_family.husbandName = "Morgan Sue"
+        test_family.wifeID = "I01"
+        test_family.wifeName = "Marry Sue"
+        test_family.chidren = []
 
-         
+        person_list = [test_person1, test_person2]
+        family_list = [test_family]
+
+        self.assertEqual(US02(person_list, family_list, False), 0)
+        self.assertEqual(US03(person_list, False), 0)
+        
+        person_list[0].birthDate = "29 SEP 2021"
+        self.assertEqual(US02(person_list, family_list, False), 1)
+        self.assertEqual(US03(person_list, False), 1)
+                
+        person_list[1].birthDate = "29 SEP 2021"
+        self.assertEqual(US02(person_list, family_list, False), 2)
+        self.assertEqual(US03(person_list, False), 2)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
